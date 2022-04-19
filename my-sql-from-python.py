dbbf956438e512a43dfc4712b74bf922da72fdf2
benjosephjones07 <benjosephjones07@gmail.com>
2022-04-19 08:43:38 +0000

    add table to chinook database through python

--- my-sql-from-python.py
+++ my-sql-from-python.py
@@ -1,22 +1,22 @@
 import os
+import datetime
 import pymysql
 
-#Get username from Cloud 9 workspace
-#Modify this variable if running on another environment
-
+# Get the username from the Cloud9 workspace
+# (modify this variable if running on another environment)
 username = os.getenv('C9_USER')
 
-#Connect to the database
-
-connection = pymysql.connect(host = 'localhost', user = username, password = '', db = 'Chinook')
+# Connect to the database
+connection = pymysql.connect(host='localhost',
+                             user=username,
+                             password='',
+                             db='Chinook')
 
 try:
-    #Run a query
     with connection.cursor() as cursor:
-        sql = "SELECT * FROM Artist;"
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        print(result)
+        cursor.execute("""CREATE TABLE IF NOT EXISTS
+                          Friends(name char(20), age int, DOB datetime);""")
+        # Note that the above will still display a warning (not error) if the
+        # table already exists
 finally:
-    #Close the connection to sql, regardless of whether the above was successful
     connection.close()
